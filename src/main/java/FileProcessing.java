9aa24b2d8e1de1fdbcfef036e9c2450bcbcec0fe
julia_kramarenko <kramarenko104@gmail.com>
2018-12-23 21:34:35 +0200

    update read/write functionality for file 'nouns.txt'

--- src/main/java/FileProcessing.java
+++ src/main/java/FileProcessing.java
@@ -1,6 +1,4 @@
 import java.io.*;
-import java.util.HashSet;
-import java.util.Set;
 
 public class FileProcessing {
 
@@ -8,33 +6,61 @@ public class FileProcessing {
 
     public FileProcessing(String filePath) {
         this.filePath = filePath;
+
+        // for testing only /////
+        try (FileWriter writer = new FileWriter(filePath, false)) {
+            writer.write("");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        ////////////////////////
     }
 
-    public Set<String> getNouns() {
-        Set<String> nouns = new HashSet<>();
-        StringBuilder noun = new StringBuilder();
-        try (FileReader reader = new FileReader(filePath)) {
-            int c;
-            while((c=reader.read())!=-1){
-                noun.append((char)c);
-            }
-            nouns.add(noun.toString());
-        } catch (FileNotFoundException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
-            e1.printStackTrace();
+    private boolean wordIsPresentInFile(String searchWord) {
+        String[] lines = null;
+        try (FileInputStream fis = new FileInputStream(new File(filePath))){
+            byte[] content = new byte[fis.available()];
+            fis.read(content);
+            lines = new String(content, "UTF-8").split("\n");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        return nouns;
+
+        for (String noun : lines) {
+            if (noun.equalsIgnoreCase(searchWord)) {
+                //System.out.println("Already present: " + searchWord);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String getRandomWordFromFile() {
+        String[] lines = null;
+        try (FileInputStream fis = new FileInputStream(new File(filePath))){
+            byte[] content = new byte[fis.available()];
+            fis.read(content);
+            lines = new String(content, "UTF-8").split("\n");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int pos = (int) (Math.random() * lines.length);
+        return lines[pos];
     }
 
     public void addNoun(String noun) {
-        try(FileWriter writer = new FileWriter(filePath, true)) {
-            writer.write(new StringBuilder(noun).append(" ").toString());
-            writer.flush();
-        } catch (IOException e1) {
-            e1.printStackTrace();
+        if (!wordIsPresentInFile(noun)) {
+            try (FileWriter writer = new FileWriter(filePath, true)) {
+                writer.write(new StringBuilder(noun).append("\n").toString());
+                writer.flush();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
         }
-
     }
 }
 
--- src/main/java/RunApp.java
+++ src/main/java/RunApp.java
@@ -5,12 +5,22 @@ public class RunApp {
     public static void main(String[] args) {
 
         String NOUNS_FILE_PATH = Paths.get(".").toAbsolutePath().normalize().toString() + "/src/main/resources/nouns.txt";
-        System.out.println(NOUNS_FILE_PATH);
         FileProcessing fileProcess = new FileProcessing(NOUNS_FILE_PATH);
 
-        fileProcess.addNoun("Маша");
-        System.out.println(fileProcess.getNouns().toString());
-        fileProcess.addNoun("Таня");
-        System.out.println(fileProcess.getNouns().toString());
+        fileProcess.addNoun("девочка Маша");
+        fileProcess.addNoun("программист Сергей");
+        fileProcess.addNoun("МарьИванна");
+        fileProcess.addNoun("депутат Иван Петрович Видпочивайло");
+        fileProcess.addNoun("сантехник Василий");
+        fileProcess.addNoun("кот Эдуард");
+        fileProcess.addNoun("сантехник Василий");
+        fileProcess.addNoun("диктор телевидения Евгений");
+        fileProcess.addNoun("мой начальник");
+        fileProcess.addNoun("девочка Маша");
+        fileProcess.addNoun("сосед-дебошир Гоша");
+        fileProcess.addNoun("слон в зоопарке");
+        fileProcess.addNoun("загадочный мужчина в кепке");
+        fileProcess.addNoun("дама в Мерседесе");
+        System.out.println("RANDOM name: "+ fileProcess.getRandomWordFromFile());
     }
 }
