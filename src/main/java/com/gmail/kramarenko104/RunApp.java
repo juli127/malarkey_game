459e835a356890351c9f44c518ebdad97b7a17a3
Iuliia Kramarenko <juli127@bigmir.net>
2019-01-23 21:05:26 +0200

    clean code

--- src/main/java/com/gmail/kramarenko104/RunApp.java
+++ src/main/java/com/gmail/kramarenko104/RunApp.java
@@ -75,54 +75,47 @@ public class RunApp extends Application {
         // buttons' actions
         for (Node node : gamePane.getChildren()) {
             if (node.getStyleClass().toString().equals("button") && node.getId() != null) {
-                String btnName = node.getId();
-
-                if (btnName.equals("btnStartGame") || btnName.equals("btnWho") ||
-                        btnName.equals("btnWhatDoes") || btnName.equals("btnWhere") ||
-                        btnName.equals("btnWhy") || btnName.equals("btnExit") ||
-                        btnName.equals("btnMusic")) {
-                    ((Button) node).setOnAction(new EventHandler<ActionEvent>() {
-                        @Override
-                        public void handle(ActionEvent event) {
-                            switch (node.getId()) {
-                                case "btnStartGame":
-                                    String newSentence = game.createSentence();
-                                    sentencesText.appendText(newSentence + "\n");
-                                    break;
-                                case "btnWho":
-                                    game.getWho().addWord(whoText.getText());
-                                    whoText.clear();
-                                    break;
-                                case "btnWhatDoes":
-                                    game.getWhatDoes().addWord(whatDoesText.getText());
-                                    whatDoesText.clear();
-                                    break;
-                                case "btnWhere":
-                                    game.getWhere().addWord(whereText.getText());
-                                    whereText.clear();
-                                    break;
-                                case "btnWhy":
-                                    game.getWhy().addWord(whyText.getText());
-                                    whyText.clear();
-                                    break;
-                                case "btnExit":
-                                    game.exit();
-                                    break;
-                                case "btnMusic":
-                                    Button btnMus = (Button) node;
-                                    String btnMusText = btnMus.getText();
-                                    if (btnMusText.equals("Play music")) {
-                                        game.playMusic(true);
-                                        btnMus.setText("Stop music");
-                                    } else if (btnMusText.equals("Stop music")) {
-                                        game.playMusic(false);
-                                        btnMus.setText("Play music");
-                                    }
-                                    break;
-                            }
+                ((Button) node).setOnAction(new EventHandler<ActionEvent>() {
+                    @Override
+                    public void handle(ActionEvent event) {
+                        switch (node.getId()) {
+                            case "btnStartGame":
+                                String newSentence = game.createSentence();
+                                sentencesText.appendText(newSentence + "\n");
+                                break;
+                            case "btnWho":
+                                game.getWho().addWord(whoText.getText());
+                                whoText.clear();
+                                break;
+                            case "btnWhatDoes":
+                                game.getWhatDoes().addWord(whatDoesText.getText());
+                                whatDoesText.clear();
+                                break;
+                            case "btnWhere":
+                                game.getWhere().addWord(whereText.getText());
+                                whereText.clear();
+                                break;
+                            case "btnWhy":
+                                game.getWhy().addWord(whyText.getText());
+                                whyText.clear();
+                                break;
+                            case "btnExit":
+                                game.exit();
+                                break;
+                            case "btnMusic":
+                                Button btnMus = (Button) node;
+                                String btnMusText = btnMus.getText();
+                                if (btnMusText.equals("Play music")) {
+                                    game.playMusic(true);
+                                    btnMus.setText("Stop music");
+                                } else if (btnMusText.equals("Stop music")) {
+                                    game.playMusic(false);
+                                    btnMus.setText("Play music");
+                                }
+                                break;
                         }
-                    });
-                }
+                    }
+                });
             }
         }
     }
